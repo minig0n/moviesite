dcf38744aec53fd40bd536ce714dacd470e9baea
alessandro <alecmenegon@gmail.com>
2023-09-25 17:57:56 -0300

    Inserir listagem, busca e criação de filmes

--- movies/urls.py
+++ movies/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'movies'
 urlpatterns = [
     path('', views.list_movies, name='index'), # adicione esta linha
+    path('search/', views.search_movies, name='search'), # adicione esta linha
     path('<int:movie_id>/', views.detail_movie, name='detail'), # adicione esta linha
+    path('create/', views.create_movie, name='create'), # adicione esta linha
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from .temp_data import movie_data
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def detail_movie(request, movie_id):
     movie = movie_data[movie_id - 1]
@@ -16,3 +18,25 @@ def detail_movie(request, movie_id):
     context = {'movie': movie_data[movie_id - 1]}
     return render(request, 'movies/detail.html', context)
 
+def search_movies(request):
+    context = {}
+    if request.GET.get('query', False):
+        context = {
+            "movie_list": [
+                m for m in movie_data
+                if request.GET['query'].lower() in m['name'].lower()
+            ]
+        }
+    return render(request, 'movies/search.html', context) # modifique esta linha
+
+def create_movie(request):
+    if request.method == 'POST':
+        movie_data.append({
+            'name': request.POST['name'],
+            'release_year': request.POST['release_year'],
+            'poster_url': request.POST['poster_url']
+        })
+        return HttpResponseRedirect(
+            reverse('movies:detail', args=(len(movie_data), )))
+    else:
+        return render(request, 'movies/create.html', {})
