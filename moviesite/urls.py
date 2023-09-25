15154e7f2eab7cf8339cfeecf8f94d9e5238b21a
alessandro <alecmenegon@gmail.com>
2023-09-25 15:25:28 -0300

    Criar site estático com Django

--- moviesite/urls.py
+++ moviesite/urls.py
@@ -14,17 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
 
 from django.contrib import admin
-from django.urls import include, path # modificar esta linha
+from django.urls import include, path
 
 urlpatterns = [
-    path('staticpages/', include('staticpages.urls')), # adicionar esta linha
+    path('', include('staticpages.urls')), # modifique esta linha
     path('admin/', admin.site.urls),
 ]
+
